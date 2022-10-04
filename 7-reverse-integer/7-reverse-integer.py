289304ffdb4891425d8cae1937025a79d780fa4d
ikhrazne <75887631+ikhrazne@users.noreply.github.com>
2022-10-04 19:50:06 +0200

    Time: 46 ms (14.17%), Space: 13.3 MB (63.13%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -4,14 +4,13 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        x = str(x)
-        x = ''.join([x[len(x) - 1 - a] for a in range(len(x))])
+        x = str(x)[::-1]
         
+        if x[len(x) - 1] == '-':
+            x = '-' + x[:-1]
+            
         if x[0] == 0:
             x = x[1:]
-        
-        if x[-1] == '-':
-            x = '-' + x[:-1]
-        
+            
         return int(x) if int(x) < 2**31 and int(x) > -2 ** 31 else 0 
         
