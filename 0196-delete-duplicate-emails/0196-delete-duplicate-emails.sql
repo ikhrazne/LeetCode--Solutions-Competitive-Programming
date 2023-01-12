8c99e84a37bb70d3e737d5e9eee44c8a98dbf28d
ikhrazne <75887631+ikhrazne@users.noreply.github.com>
2023-01-12 22:54:59 +0100

    Time: 1296 ms (14.14%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,6 @@
+/*
+ Please write a DELETE statement and DO NOT write a SELECT statement.
+ Write your PL/SQL query statement below
+ */
+
+delete from person where id not in (select min(id) from person group by email);
